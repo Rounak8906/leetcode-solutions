2ceb8b7aff948488458f3ac841ee20c3c053c3e0
ROUNAK SINGH <rounaksingh8906@gmail.com>
2026-08-27 14:15:16 +0530

    [3720] [Time Beats: 100.00%] [Memory Beats: 100.00%] - LeetPush

--- 3720-Lexicographically-Smallest-Permutation-Greater-Than-Target.c
+++ 3720-Lexicographically-Smallest-Permutation-Greater-Than-Target.c
@@ -8,81 +8,82 @@ char* lexGreaterPermutation(char* s, char* target) {
 
     char* ans = (char*)malloc((n + 1) * sizeof(char));
 
-    for (int i = 0; i < n; i++) {
-        int t = target[i] - 'a';
+    int i;
 
-        /* Try to keep the same character */
-        if (count[t] > 0) {
+    /* Match target as much as possible */
+    for (i = 0; i < n; i++) {
+        int c = target[i] - 'a';
+
+        if (count[c] > 0) {
             ans[i] = target[i];
-            count[t]--;
-        } 
-        else {
-            /* Find the smallest character greater than target[i] */
-            int j;
-            for (j = t + 1; j < 26; j++) {
-                if (count[j] > 0)
-                    break;
-            }
-
-            if (j == 26) {
-                /* Need to backtrack */
-                int k = i - 1;
-
-                while (k >= 0) {
-                    count[ans[k] - 'a']++;
-
-                    int c;
-                    for (c = ans[k] - 'a' + 1; c < 26; c++) {
-                        if (count[c] > 0)
-                            break;
-                    }
-
-                    if (c < 26) {
-                        ans[k] = 'a' + c;
-                        count[c]--;
-
-                        k++;
-
-                        /* Fill remaining positions smallest first */
-                        int pos = k;
-
-                        for (int x = 0; x < 26; x++) {
-                            while (count[x] > 0) {
-                                ans[pos++] = 'a' + x;
-                                count[x]--;
-                            }
-                        }
-
-                        ans[n] = '\0';
-                        return ans;
-                    }
-
-                    k--;
-                }
-
-                free(ans);
-                return "";
-            }
-
-            ans[i] = 'a' + j;
-            count[j]--;
-
-            /* Fill the remaining characters in smallest order */
-            int pos = i + 1;
-
-            for (int x = 0; x < 26; x++) {
-                while (count[x] > 0) {
-                    ans[pos++] = 'a' + x;
-                    count[x]--;
-                }
-            }
-
-            ans[n] = '\0';
-            return ans;
+            count[c]--;
+        } else {
+            break;
         }
     }
 
-    /* s itself can only produce target, not something greater */
+    /*
+     * If we could not match target at position i,
+     * first try making THIS position bigger.
+     */
+    if (i < n) {
+        int targetChar = target[i] - 'a';
+
+        for (int c = targetChar + 1; c < 26; c++) {
+            if (count[c] > 0) {
+                ans[i] = 'a' + c;
+                count[c]--;
+
+                int pos = i + 1;
+
+                for (int x = 0; x < 26; x++) {
+                    while (count[x] > 0) {
+                        ans[pos++] = 'a' + x;
+                        count[x]--;
+                    }
+                }
+
+                ans[n] = '\0';
+                return ans;
+            }
+        }
+    }
+
+    /*
+     * If we cannot make the current position bigger,
+     * backtrack to an earlier position.
+     */
+    for (int k = i - 1; k >= 0; k--) {
+
+        /* Put the character back */
+        count[ans[k] - 'a']++;
+
+        int targetChar = target[k] - 'a';
+
+        /* Find smallest character greater than target[k] */
+        for (int c = targetChar + 1; c < 26; c++) {
+
+            if (count[c] > 0) {
+
+                ans[k] = 'a' + c;
+                count[c]--;
+
+                /* Fill rest in sorted order */
+                int pos = k + 1;
+
+                for (int x = 0; x < 26; x++) {
+                    while (count[x] > 0) {
+                        ans[pos++] = 'a' + x;
+                        count[x]--;
+                    }
+                }
+
+                ans[n] = '\0';
+                return ans;
+            }
+        }
+    }
+
     free(ans);
     return "";
 }
